9098d6e891da8177f01daa779d91202a327a4461
Andrey Lukyanchuk <andreylukyanchuk623@gmail.com>
2020-09-12 23:51:15 +0300

    added "accept" to HEADERS

--- const.py
+++ const.py
@@ -24,7 +24,8 @@ def get_dollar_rate():
 
 HEADERS = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+                  "(KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36",
+    "accept": "*/*"
 }
 
 DOLLAR_RATE = get_dollar_rate()
