60ffa8d9a553677acc1fcc5f47e872d0e77bf1d1
Andrey Lukyanchuk <andreylukyanchuk623@gmail.com>
2020-09-12 20:59:27 +0300

    final commit

--- const.py
+++ const.py
@@ -10,7 +10,6 @@ To stop process press Ctrl + c
 <------------------------------------------------------------------------------------->""")
 
 
-
 def get_dollar_rate():
     url = 'https://www.google.com/search?q=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80' \
           '+%D0%BA+%D0%B3%D1%80%D0%B8%D0%B2%D0%BD%D0%B5&oq=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80' \
